594b8ca0702da2d60049ab5469309dd54b024ea6
n3wton <n3wton@8fd278ee-d519-4082-b857-58b43967699a>
2013-01-23 18:44:12 +0000

    Updated coding standards and comments for the check box class.
    git-svn-id: https://subversion.assembla.com/svn/bupload@5 8fd278ee-d519-4082-b857-58b43967699a

--- src/minecraft/uk/codingbadgers/Gui/GuiCheckBox.java
+++ src/minecraft/uk/codingbadgers/Gui/GuiCheckBox.java
@@ -12,73 +12,131 @@ import org.lwjgl.opengl.GL11;
 @SideOnly(Side.CLIENT)
 public class GuiCheckBox extends GuiButton
 {
+	/** The current checked state of the check box */
 	private boolean m_checked = false;
 	
-    public GuiCheckBox(int par1, int par2, int par3, int par4, int par5, String par6Str)
+	/** The amount of space between the check box and its label */
+	private final static int BOX_LABEL_SPACER = 5;
+	
+	/**
+	 * Default check box constructor
+	 * @param id 				The id of the callback used in actionPerformed.
+	 * @param xPosition			The x coordinate of the position of the check box
+	 * @param yPosition			The y coordinate of the position of the check box
+	 * @param width				The width of the check box
+	 * @param height			The height of the check box
+	 * @param label				The label of the check box
+	 */
+    public GuiCheckBox(int id, int xPosition, int yPosition, int width, int height, String label)
     {
-    	super(par1, par2, par3, par4, par5, par6Str);
+    	super(id, xPosition, yPosition, width, height, label);
     }
 
     /**
-     * Returns 0 if the button is disabled, 1 if the mouse is NOT hovering over this button and 2 if it IS hovering over
-     * this button.
+     * Returns 0 if the check box is disabled, 
+     * 1 if the mouse is NOT hovering over this check box 
+     * and 2 if it is hovering over this check box.
      */
-    protected int getHoverState(boolean par1)
+    protected int getHoverState(boolean isMouseOver)
     {
-        byte var2 = 1;
+        if (!enabled)
+            return 0;
+        
+        if (isMouseOver)
+            return 2;
 
-        if (!this.enabled)
-        {
-            var2 = 0;
-        }
-        else if (par1)
-        {
-            var2 = 2;
-        }
-
-        return var2;
+        return 1;
     }
     
+    /**
+     * Sets the current checked state of the check box
+     * @param check			True to set the checked state to true, false otherwise
+     */
     public void setChecked(boolean check) {
     	m_checked = check;
     }
     
+    /**
+     * Get the current checked state of the check box
+     * @return True if checked, false otherwise
+     */
     public boolean getChecked() {
     	return m_checked;
     }
 
     /**
-     * Draws this button to the screen.
+     * Draws the check box to the screen.
+     * @param minecraft				The minecraft instance
+     * @param mouseX				The x coordinate of the mouse
+     * @param mouseY				The y coordinate of the mouse
      */
-    public void drawButton(Minecraft par1Minecraft, int par2, int par3)
+    public void drawButton(Minecraft minecraft, int mouseX, int mouseY)
     {
-        if (this.drawButton)
-        {
-            GL11.glBindTexture(GL11.GL_TEXTURE_2D, par1Minecraft.renderEngine.getTexture("/gui/tcb-gui.png"));
-            GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-            
-            this.field_82253_i = par2 >= this.xPosition && par3 >= this.yPosition && par2 < this.xPosition + this.width && par3 < this.yPosition + this.height;
-            int var5 = this.getHoverState(this.field_82253_i);
-            
-            //this.drawTexturedModalRect(this.xPosition, this.yPosition, 0, 46 + var5 * 20, this.width / 2, this.height);
-            //this.drawTexturedModalRect(this.xPosition + this.width / 2, this.yPosition, 200 - this.width / 2, 46 + var5 * 20, this.width / 2, this.height);
-            
-            int localYoffset = 166;
-            if (var5 == 2) localYoffset = 186;
-            if (m_checked) localYoffset = 146;
-            
-            this.mouseDragged(par1Minecraft, par2, par3);
-            int var6 = 14737632;
-            if (!enabled) var6 = -6250336; else if (field_82253_i) var6 = 16777120;
-            
-            int strlen = par1Minecraft.fontRenderer.getStringWidth(displayString);
-            int extra = (width - strlen) / 4;
-            
-            this.drawTexturedModalRect(xPosition + extra, yPosition, 0, localYoffset, 20, 20);
-            this.drawCenteredString(par1Minecraft.fontRenderer, this.displayString, (this.xPosition + this.width / 2) + extra, this.yPosition + (this.height - 8) / 2, var6);
-        }
+        if (!drawButton)
+        	return;
+        
+        // load our check box image
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, minecraft.renderEngine.getTexture("/gui/tcb-gui.png"));
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        
+        // field_82253_i represents if the mouse is over the check box region
+        field_82253_i = mouseX >= this.xPosition && mouseY >= this.yPosition && mouseX < this.xPosition + this.width && mouseY < this.yPosition + this.height;
+       
+        // get the hover state of the mouse and check box
+        final int hoverState = getHoverState(field_82253_i);
+
+        // work out the local offset into the image atlas
+        int localYoffset = 166;
+        if (hoverState == 2) localYoffset = 186;
+        if (m_checked) localYoffset = 146;
+        
+        // get a color offset based upon the hover state
+        final int hoverColor = enabled == false ? -6250336 : hoverState == 2 ? 16777120 : 14737632;
+        
+        // work out an offset to add the check box image and center the check box
+        final int labelWidth = minecraft.fontRenderer.getStringWidth(displayString);
+        final int checkboxImageSize = 20; 
+        final int xOffset = xPosition + ((width / 2) - ((labelWidth + checkboxImageSize + BOX_LABEL_SPACER) / 2));
+        
+        // draw the check box
+        drawTexturedModalRect(xOffset, yPosition, 0, localYoffset, checkboxImageSize, checkboxImageSize);
+        
+        // draw the check box label
+        drawString(minecraft.fontRenderer, displayString, xOffset + checkboxImageSize + BOX_LABEL_SPACER, yPosition + (height - 8) / 2, hoverColor);
     }
 
+    /**
+     * Handle a mouse pressed event
+     * @param minecraft			The minecraft instance
+     * @param mouseX			The x coordinate of the mouse
+     * @param mouseY			The y coordinate of the mouse
+     * 
+     * @return True if the mouse was pressed whilst over the check box, false otherwise
+     */
+    public boolean mousePressed(Minecraft minecraft, int mouseX, int mouseY)
+    {
+    	if (!enabled)
+    		return false;
+    	
+    	if (!drawButton)
+    		return false;
+    		
+    	boolean pressed = mouseX >= xPosition && mouseY >= yPosition && mouseX < xPosition + width && mouseY < yPosition + height;    	
+        
+    	// Change the check state as the check box has been clicked
+    	if (pressed) m_checked = !m_checked;
+    	
+    	return pressed;
+    }
+
+    /**
+     * @return if the mouse is currently over the check box
+     */
+    public boolean func_82252_a()
+    {
+        return this.field_82253_i;
+    }
+    
     /**
      * Fired when the mouse button is dragged. Equivalent of MouseListener.mouseDragged(MouseEvent e).
      */
@@ -87,28 +145,11 @@ public class GuiCheckBox extends GuiButton
     /**
      * Fired when the mouse button is released. Equivalent of MouseListener.mouseReleased(MouseEvent e).
      */
-    public void mouseReleased(int par1, int par2) 
-    {
-    }
+    public void mouseReleased(int par1, int par2) {}
 
     /**
-     * Returns true if the mouse has been pressed on this control. Equivalent of MouseListener.mousePressed(MouseEvent
-     * e).
+     * 
      */
-    public boolean mousePressed(Minecraft par1Minecraft, int par2, int par3)
-    {
-    	boolean pressed = this.enabled && this.drawButton && par2 >= this.xPosition && par3 >= this.yPosition && par2 < this.xPosition + this.width && par3 < this.yPosition + this.height;    	
-        
-    	if (pressed) m_checked = !m_checked;
-    	
-    	return pressed;
-    }
-
-    public boolean func_82252_a()
-    {
-        return this.field_82253_i;
-    }
-
     public void func_82251_b(int par1, int par2) {}
 }
 
